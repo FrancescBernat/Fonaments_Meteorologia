1e5f7f4cdb6688ee50ae3af0aaa7aba0e58e3ffa
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-14 15:38:23 +0100

    Es llegeix la latitud i la longitud de manera correcta.

--- P1_ProvaLectura.py
+++ P1_ProvaLectura.py
@@ -14,18 +14,28 @@ import matplotlib.pyplot as plt
 
 arxiu = 'AQUA_MODIS.20200102T125001.L2.SST.nc'
 
+# Llegim arxiu nc
 data = nc.Dataset('DadesMar/'+ arxiu, 'r')
 # print(data.groups)
 
 Geo = data.groups['geophysical_data']
 print(Geo.variables.keys())
 
+# Extreim informació del dia
 info = data['scan_line_attributes']
-
 dia = str(info['day'][0])
 
+# Extreim les coordenades
+nav = data['navigation_data']
+lat = np.fliplr(nav['latitude'][:])
+lon = np.fliplr(nav['longitude'][:])
+
+# Extreim i ordenam correctament les dades sst
 sst = np.fliplr(Geo.variables['sst'][:])
-plt.contour(sst)
+
+fig, ax = plt.subplots(figsize=(9, 7), dpi=400)
+ax.contour(lon, lat, sst)
+ax.set_title(dia)
 plt.show()
 
 
