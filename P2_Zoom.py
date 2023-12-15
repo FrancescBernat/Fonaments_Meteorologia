fae3418805247faaa41b41ec415d8158ab4fb7d1
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-15 12:23:30 +0100

    Zoom Complet a l'arxiu P2

--- P2_Zoom.py
+++ P2_Zoom.py
@@ -38,52 +38,19 @@ sst = np.fliplr(Geo.variables['sst'][:])
 nav = data['navigation_data']
 lat = np.fliplr(nav['latitude'][:])
 lon = np.fliplr(nav['longitude'][:])
-ind2 = np.argwhere((lat>=37) & (lat<=42) & (lon>=0) & (lon<=6))
-
-ind = (lat>=37) & (lat<=42) & (lon>=0) & (lon<=6)
-
-indlat = np.where((lat>37) & (lat<42))[0]
-indlon = np.where((lon>0) & (lon<6))[0]
-
-# Problema, les latituds i les longituds cambien
-indlat1 = np.where(lat[:,0].data>=37)[0][0]
-indlat2 = np.where(lat[:,0].data<=42)[0][-1]
-
-# Problema, les latituds i les longituds cambien
-indlon1 = np.where(lon[1,:].data>1)[0][0]
-indlon2 = np.where(lon[1,:].data<6)[0][-1]
-
-# sst = sst[indlat[0]:indlat[-1], indlon[0]:indlon[-1]]
-# lat = lat[indlat[0]:indlat[-1], indlon[0]:indlon[-1]]
-
-# lon = lon[indlat[0]:indlat[-1], indlon[0]:indlon[-1]]
-# lat = lat[indlat]
-# lon = lon[indlon]
-# sst = sst[indlat][indlon]
-
-# sst = sst[indlat1:indlat2, indlon1:indlon2]
-# lon = lon[indlat1:indlat2, indlon1:indlon2]
-# lat = lat[indlat1:indlat2, indlon1:indlon2]
-
-# sst = sst[ind2]
-# lat = lat[ind2]
-# lon = lon[ind2]
 
 min_lon = 1
-min_lat = 37
 max_lon = 6
+
+min_lat = 37
 max_lat = 42
 
-da = xr.DataArray([lon, lat, sst], dims=['lon', 'lat', 'sst'])
 da = xr.DataArray(sst, dims=['x', 'y'], 
                   coords = dict(lon=(["x", "y"], lon), 
                             lat=(["x", "y"], lat)))
 
 mask_lon = (da.lon >= min_lon) & (da.lon <= max_lon)
 mask_lat = (da.lat >= min_lat) & (da.lat <= max_lat)
-# a = da.isel(lat=slice(37,42), lon=slice(0,6))
-
-# cropped_ds = da.sel(lat=slice(min_lat,max_lat), lon=slice(min_lon,max_lon))
 
 cro_da = da.where(mask_lon & mask_lat, drop=True)
 
@@ -97,14 +64,17 @@ ax.add_feature(cart.feature.LAND, zorder=2, edgecolor='k', linewidth=0.05)
 plot = ax.contourf(cro_da.lon, cro_da.lat, cro_da, 70, transform=cart.crs.PlateCarree())
 
 # Ticks per a la longitud
-ax.set_xticks(np.arange(np.min(cro_da.lon[:]), np.max(cro_da.lon[:]), 6), crs=cart.crs.PlateCarree())
+ax.set_xticks(np.linspace(np.min(cro_da.lon[:]), np.max(cro_da.lon[:]), 5), crs=cart.crs.PlateCarree())
 lon_formatter = LongitudeFormatter(number_format='.2f')
 ax.xaxis.set_major_formatter(lon_formatter)
 
 # Ticks per a la latitud
-ax.set_yticks(np.arange(np.min(cro_da.lat[:]), np.max(cro_da.lat[:]), 5), crs=cart.crs.PlateCarree())
+ax.set_yticks(np.linspace(np.min(cro_da.lat[:]), np.max(cro_da.lat[:]), 5), crs=cart.crs.PlateCarree())
 lat_formatter = LatitudeFormatter(number_format='.2f')
 ax.yaxis.set_major_formatter(lat_formatter)
+
+ax.set_xlim([min_lon, max_lon])
+ax.set_ylim([min_lat, max_lat])
 
 cb = plt.colorbar(plot)
 cb.set_label("sst (ºC)", rotation=270)
