d5813bfa2d436c74a8324919a84c0540e6bad5c5
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-18 16:36:43 +0100

    Creat un arxiu que crea tres grafics per a totes les dades seleccionades.

--- funcions.py
+++ funcions.py
@@ -108,3 +108,45 @@ def Repr(PlotData, lon_Loc, lat_Loc, Zona, T):
     cb.set_label("sst (ºC)", rotation=270)
     ax.set_title(f"Dades sst pel dia {T} a {Zona}")
     plt.show()
+
+def GuardGraf(PlotData, lon_Loc, lat_Loc, Zona, T):
+    '''
+    Funció per a guardar la representació d'un contorn del sst de l'area de
+    interes.
+    '''
+
+    # Per a cambiar les lletres a l'estil que és te a latex
+    mp.rcParams['mathtext.fontset'] = 'stix'
+    mp.rcParams['font.family'] = 'STIXGeneral'
+    mp.rcParams.update({'font.size': 14})
+
+    # Extreim les longituds i latituds mínimes i maximes
+    min_lon, max_lon = lon_Loc
+    min_lat, max_lat = lat_Loc
+
+    fig = plt.figure(figsize=(9, 7), dpi=400)
+    ax = plt.axes(projection=cart.crs.PlateCarree())
+    ax.coastlines()
+    ax.add_feature(cart.feature.LAND, zorder=2, edgecolor='k', linewidth=0.05)
+
+    plot = ax.contourf(PlotData.lon, PlotData.lat, PlotData, 70, 
+                        transform=cart.crs.PlateCarree(), cmap='RdYlBu_r')
+
+    # Ticks per a la longitud
+    ax.set_xticks(np.linspace(min_lon, max_lon, 5), crs=cart.crs.PlateCarree())
+    lon_formatter = LongitudeFormatter(number_format='.2f')
+    ax.xaxis.set_major_formatter(lon_formatter)
+
+    # Ticks per a la latitud
+    ax.set_yticks(np.linspace(min_lat, max_lat, 5), crs=cart.crs.PlateCarree())
+    lat_formatter = LatitudeFormatter(number_format='.2f')
+    ax.yaxis.set_major_formatter(lat_formatter)
+
+    ax.set_xlim([min_lon, max_lon])
+    ax.set_ylim([min_lat, max_lat])
+
+    cb = plt.colorbar(plot)
+    cb.set_label("sst (ºC)", rotation=270)
+    ax.set_title(f"Dades sst pel dia {T}")
+    date  = T.strftime("%Y_%m_%d_%H_%M_%S")
+    plt.savefig( Zona +'/'+ date +'.png', dpi=450)
