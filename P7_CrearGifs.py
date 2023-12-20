62de6ee66ac2f03999a504398c5abda638ccbccd
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-20 16:21:48 +0100

    Creat una funció per a la creació de gifs.

---
+++ P7_CrearGifs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   P7_CrearGifs.py
+@Date    :   2023/12/20 16:12:11
+@Author  :   Francesc Bernat Bieri Tauler 
+@Contact :   franz@canmenut.com
+@Version :   1.0
+'''
+
+import os
+import numpy
+import glob
+import imageio
+
+subfolders = ['IB', 'CMe',  'CMa', 'Glob']
+folders = ['Imatges/'+i for i in subfolders]
+
+filenames = glob.glob(folders[0]+"/*.png")
+
+images = []
+for filename in filenames:
+    images.append(imageio.imread(filename))
+imageio.mimsave('movie.gif', images, duration=2)
