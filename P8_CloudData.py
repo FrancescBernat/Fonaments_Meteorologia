0c47394d92560075e2f8ca4294f9886afc7be103
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-21 16:25:18 +0100

    Afegit un arxiu per a analitzar la presencia de niguls.

---
+++ P8_CloudData.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   P8_CloudData.py
+@Date    :   2023/12/21 16:21:52
+@Author  :   Francesc Bernat Bieri Tauler 
+@Contact :   franz@canmenut.com
+@Version :   1.0
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
