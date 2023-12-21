4fb78f73abb032666c64197dec43e1ae680ccda6
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-21 17:07:59 +0100

    Grafiques pel total de niguls.

--- P8_CloudData.py
+++ P8_CloudData.py
@@ -14,12 +14,20 @@ import datetime as dt
 import cartopy as cart
 import matplotlib.pyplot as plt
 
+from cartopy.mpl.ticker import LatitudeFormatter, LongitudeFormatter
+
+
 arxiu = "Clouds.nc"
 
 data = nc.Dataset(arxiu, 'r')
 
 time = data['time'][:]
+t = time[0]
 start = dt.datetime(1990,1,1,0,0,0)   
+# t = dt.datenum(1900,1,1,np.double(t),0,0)
+# dt.datetime(1900,1,1,0,0,0)+dt.timedelta(hours=int(t))
+T = [dt.datetime(1900,1,1,0,0,0)+dt.timedelta(hours=int(t)) 
+     for t in time]
 
 lat = data['latitude'][:]
 lon = data['longitude'][:]
@@ -34,31 +42,27 @@ cbh = data['cbh'][:]
 lcc = data['lcc'][:]
 
 
-# for i in range(5):
-T = 0;
+for i in range(len(T)):
 
-fig = plt.figure(figsize=(9, 7), dpi=400)
-ax = plt.axes(projection=cart.crs.PlateCarree())
-ax.coastlines()
-ax.add_feature(cart.feature.LAND, zorder=2, edgecolor='k', linewidth=0.05)
+    fig = plt.figure(figsize=(9, 7), dpi=400)
+    ax = plt.axes(projection=cart.crs.PlateCarree())
+    ax.coastlines()
+    ax.add_feature(cart.feature.LAND, zorder=2, edgecolor='k', linewidth=0.05)
 
-plot = ax.contourf(lon, lat, tcc[0], 70, transform=cart.crs.PlateCarree(),
-                   cmap='jet')# cmap='RdYlBu_r')
+    plot = ax.contourf(lon, lat, tcc[i], 70, transform=cart.crs.PlateCarree(),
+                    cmap='jet')# cmap='RdYlBu_r')
 
-# # Ticks per a la longitud
-# ax.set_xticks(np.linspace(min_lon, max_lon, 5), crs=cart.crs.PlateCarree())
-# lon_formatter = LongitudeFormatter(number_format='.2f')
-# ax.xaxis.set_major_formatter(lon_formatter)
+    # Ticks per a la longitud
+    ax.set_xticks(lon[::3], crs=cart.crs.PlateCarree())
+    lon_formatter = LongitudeFormatter(number_format='.2f')
+    ax.xaxis.set_major_formatter(lon_formatter)
 
-# # Ticks per a la latitud
-# ax.set_yticks(np.linspace(min_lat, max_lat, 5), crs=cart.crs.PlateCarree())
-# lat_formatter = LatitudeFormatter(number_format='.2f')
-# ax.yaxis.set_major_formatter(lat_formatter)
+    # Ticks per a la latitud
+    ax.set_yticks(lat[::3], crs=cart.crs.PlateCarree())
+    lat_formatter = LatitudeFormatter(number_format='.2f')
+    ax.yaxis.set_major_formatter(lat_formatter)
 
-# ax.set_xlim([min_lon, max_lon])
-# ax.set_ylim([min_lat, max_lat])
-
-cb = plt.colorbar(plot)
-cb.set_label("sst (ºC)", rotation=270)
-ax.set_title(f"Dades sst pel dia {T}")
-plt.show()
+    cb = plt.colorbar(plot)
+    cb.set_label("Total Cloud Cover")
+    ax.set_title(f"Total Cloud Cover pel dia {T[i]}")
+    plt.show()
