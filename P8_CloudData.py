4af45ff4def59284600b515ea30ecf23bd97a00f
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2023-12-21 17:19:35 +0100

    Canvi del titol

--- P8_CloudData.py
+++ P8_CloudData.py
@@ -39,17 +39,16 @@ cbh = data['cbh'][:]
 # low cloud cover
 lcc = data['lcc'][:]
 
-
 for i in range(len(T)):
 
-    fig = plt.figure(figsize=(9, 7), dpi=400)
+    fig = plt.figure(figsize=(9, 7), dpi=600)
     ax = plt.axes(projection=cart.crs.PlateCarree())
     ax.coastlines()
     ax.add_feature(cart.feature.LAND, zorder=2, edgecolor='k', linewidth=0.05)
 
     plot = ax.contourf(lon, lat, tcc[i], 70, transform=cart.crs.PlateCarree(),
-                    cmap=cm.cm.rain)# cmap='RdYlBu_r')
-
+                    cmap=cm.cm.rain)
+    
     # Ticks per a la longitud
     ax.set_xticks(lon[::3], crs=cart.crs.PlateCarree())
     lon_formatter = LongitudeFormatter(number_format='.2f')
@@ -61,6 +60,6 @@ for i in range(len(T)):
     ax.yaxis.set_major_formatter(lat_formatter)
 
     cb = plt.colorbar(plot)
-    cb.set_label("Total Cloud Cover")
-    ax.set_title(f"Total Cloud Cover pel dia {T[i]}")
+    cb.set_label("Percentatge de niguls")
+    ax.set_title(f'Niguls a les {T[i].strftime("%H:%M:%S")}')
     plt.show()
